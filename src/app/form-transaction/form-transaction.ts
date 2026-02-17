36d74aac36ef227972741cb97fdb78a21cbb4942
carlosthompson <carlosethompson@gmail.com>
2026-02-17 14:10:33 +0000

    feat: improve form and update style

--- src/app/form-transaction/form-transaction.ts
+++ src/app/form-transaction/form-transaction.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form-transaction',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './form-transaction.html',
   styleUrl: './form-transaction.css',
 })
 export class FormTransaction {
+  valueTransaction = '';
+  transactionType = '';
 
+  onSubmit() {
+    console.log(this.transactionType);
+    console.log(this.valueTransaction);
+  }
 }
